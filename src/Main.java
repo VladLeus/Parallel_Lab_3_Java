94fb8ea3e01b1b9edd4a0272bb52fbf7a70be71c
VladLeus <vladyslavleus@gmail.com>
2024-04-11 22:43:48 +0300

    Corrected Semaphore

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@
 public class Main {
     public static void main(String[] args) {
         final int capacity = 10;
-        Storage storage = new StorageSemaphore(capacity);
+        Storage storage = new StorageWait(capacity);
+        /*Storage storage = new StorageSemaphore(capacity);*/
 
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < capacity; i++) {
             new Producer(i, storage).start();
             new Consumer(i, storage).start();
         }
--- src/StorageSemaphore.java
+++ src/StorageSemaphore.java
@@ -2,36 +2,30 @@ import java.util.LinkedList;
 import java.util.concurrent.Semaphore;
 
 public class StorageSemaphore  implements Storage {
-    private final int capacity;
-    private final LinkedList<Integer> storage;
-    private final Semaphore semaphore;
+    private final LinkedList<Integer> storage = new LinkedList<>();
+    private final Semaphore spaceAvailable;
+    private final Semaphore itemsAvailable;
 
     public StorageSemaphore(int capacity) {
-        this.capacity = capacity;
-        this.storage = new LinkedList<>();
-        this.semaphore = new Semaphore(capacity);
+        this.spaceAvailable = new Semaphore(capacity);
+        this.itemsAvailable = new Semaphore(0);
     }
 
     public void add(Integer elem) throws InterruptedException {
-        semaphore.acquire();  // Acquire a permit
+        spaceAvailable.acquire();
         synchronized (this) {
             storage.add(elem);
-            System.out.println("Producer added element: " + elem);
-            notifyAll();
         }
+        itemsAvailable.release();
     }
 
     public Integer poll() throws InterruptedException {
+        itemsAvailable.acquire();
         Integer element;
         synchronized (this) {
-            while (storage.isEmpty()) {
-                wait();
-            }
             element = storage.poll();
-            System.out.println("Consumer removed element: " + element);
-            notifyAll();
         }
-        semaphore.release();  // Release a permit
+        spaceAvailable.release();
         return element;
     }
 }
