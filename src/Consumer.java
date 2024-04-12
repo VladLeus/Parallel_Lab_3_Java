4a3f4f23f9fb619312819ce17386af3774e99e19
VladLeus <vladyslavleus@gmail.com>
2024-04-12 12:30:12 +0300

    fixed consumer

--- src/Consumer.java
+++ src/Consumer.java
@@ -2,16 +2,19 @@ public class Consumer extends Thread {
     private final long id;
     private final Storage storage;
 
+    private final int itemsToConsume;
+
 
     public Consumer(long id, Storage storage, int itemsToConsume) {
         this.id = id;
         this.storage = storage;
+        this.itemsToConsume = itemsToConsume;
     }
 
     @Override
     public void run() {
         try {
-            while (!storage.getStorage().isEmpty()) {
+            for (int i = 0; i < itemsToConsume; i++) {
                 int element = storage.poll();
                 Thread.sleep(1000);
                 System.out.println("Consumer "+ id + " removed element: " + element);
--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,18 @@ public class Main {
     public static void main(String[] args) {
         final int capacity = 7;
         final int items = 10;
+        final int producersAmount = 5;
+        final int consumersAmount = 3;
         Storage storage = new StorageWait(capacity);
         /*Storage storage = new StorageSemaphore(capacity);*/
 
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < producersAmount; i++) {
             new Producer(i, storage, items).start();
         }
 
-        for (int i = 0; i < 3; i++) {
-            new Consumer(i, storage, items).start();
+        for (int i = 0; i < consumersAmount; i++) {
+            new Consumer(i, storage, (items * producersAmount) / consumersAmount).start();
         }
     }
 }
